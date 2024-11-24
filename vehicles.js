9442d55eff3b4f4bce3f88ddb8f9b10e11c63e1f
Jason Weeks <49565288+ChandlerWeeks@users.noreply.github.com>
2024-11-23 22:02:55 -0600

    added navbar

--- vehicles.js
+++ vehicles.js
@@ -2,7 +2,6 @@
 fetch("./data/vehicles.json")
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
     addVehiclesToPage(data.companyVehicles); // Pass the JSON data to the function
   })
   .catch((error) => {
@@ -11,9 +10,15 @@ fetch("./data/vehicles.json")
 
 // Function to create and add vehicle divs to the page
 function addVehiclesToPage(vehicles) {
-  const vehicleContainer = document.getElementById("vehicles-container");
+  const vehicleContainer = document.getElementById("card-container");
 
   vehicles.forEach((vehicle) => {
+    // define data
+    var in_service;
+    var alert_color;
+    var bg_color;
+    var warning_text;
+
     // process data for the card
     if (vehicle.in_service) {
       in_service = "Yes";
@@ -21,25 +26,39 @@ function addVehiclesToPage(vehicles) {
       in_service = "No";
     }
 
-    if (vehicle.alerts.length === 0) {
-      console.log("No alerts");
-    } else if (vehicle.alerts.length === 1) {
-      console.log("one alert");
-    } else {
-      console.log("several alerts");
+    // Process the alert coloration and message
+    if (vehicle.alert_level == "none") {
+      alert_color = "border-secondary";
+      bg_color = "bg-secondary";
+      warning_text = "No Issues";
+    } else if (vehicle.alert_level == "caution") {
+      alert_color = "border-warning";
+      bg_color = "bg-warning";
+      warning_text =
+        vehicle.alerts.length > 1
+          ? "Multiple Warnings"
+          : `Warning - ${vehicle.alerts[0]}`;
+    } else if (vehicle.alert_level == "critical") {
+      alert_color = "border-danger";
+      bg_color = "bg-danger";
+      warning_text =
+        vehicle.alerts.length > 1
+          ? "Multiple Failures"
+          : `Failure - ${vehicle.alerts[0]}`;
     }
+
+    // Create the card div
     const vehicleDiv = document.createElement("div");
-    vehicleDiv.classList.add("vehicle");
+    vehicleDiv.classList.add("card", "text-center", alert_color);
+    vehicleDiv.style.maxWidth = "16rem";
     vehicleDiv.id = `vehicle${vehicle.id}`;
     vehicleDiv.innerHTML = `
-      <div class="vehicle-frame vehicle-frame-error">
-        <div class="error-frame">
-          <p class="alert-text">Brakes - 10/21/2024</p>
-        </div>
-        <p class='card-description'><strong>${vehicle.year} ${vehicle.make} ${vehicle.model}</strong></p>
-        <p class='card-description'><strong>In Service: ${in_service}</strong></p>
-      </div>
-    `;
+    <div class="card-header ${bg_color} text-white">${warning_text}</div>
+    <div class="card-body">
+      <h5 class="card-title">${vehicle.year} ${vehicle.make} ${vehicle.model}</h5>
+      <img class="card-img-top" src="./assets/images/Asset_2.png" alt="Card image cap">
+    </div>
+  `;
     vehicleContainer.appendChild(vehicleDiv);
 
     const vehicleButtons = document.querySelectorAll(".vehicle");
