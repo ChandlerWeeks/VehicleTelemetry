f4152b848f703d9f797e71ba0af32c41a532fd24
Jason Weeks <49565288+ChandlerWeeks@users.noreply.github.com>
2024-12-09 15:25:08 -0600

    Revert "Attempt at searchbar"
    This reverts commit f4e4f09a7f634bdf7f6f04a3cca3f90da3f61f44.

--- vehicles.js
+++ vehicles.js
@@ -1,12 +1,12 @@
 // Fetch the JSON data
 fetch("./data/vehicles.json")
-    .then((response) => response.json())
-    .then((data) => {
-      addVehiclesToPage(data.companyVehicles); // Pass the JSON data to the function
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  .then((response) => response.json())
+  .then((data) => {
+    addVehiclesToPage(data.companyVehicles); // Pass the JSON data to the function
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 // Function to create and add vehicle divs to the page
 function addVehiclesToPage(vehicles) {
@@ -36,16 +36,16 @@ function addVehiclesToPage(vehicles) {
       alert_color = "border-warning";
       bg_color = "bg-warning";
       warning_text =
-          vehicle.alerts.length > 1
-              ? "Multiple Warnings"
-              : Warning - ${vehicle.alerts[0]};
+        vehicle.alerts.length > 1
+          ? "Multiple Warnings"
+          : `Warning - ${vehicle.alerts[0]}`;
     } else if (vehicle.alert_level == "critical") {
       alert_color = "border-danger";
       bg_color = "bg-danger";
       warning_text =
-          vehicle.alerts.length > 1
-              ? "Multiple Failures"
-              : Failure - ${vehicle.alerts[0]};
+        vehicle.alerts.length > 1
+          ? "Multiple Failures"
+          : `Failure - ${vehicle.alerts[0]}`;
     }
 
     //process the image based on make and model
@@ -65,22 +65,22 @@ function addVehiclesToPage(vehicles) {
     const vehicleDiv = document.createElement("div");
     vehicleDiv.classList.add("card", "text-center", alert_color);
     vehicleDiv.style.maxWidth = "16rem";
-    vehicleDiv.id = ${vehicle.id};
-    vehicleDiv.innerHTML =
-        <div class="card-header ${bg_color} text-white warning-text">${warning_text}</div>
+    vehicleDiv.id = `${vehicle.id}`;
+    vehicleDiv.innerHTML = `
+    <div class="card-header ${bg_color} text-white warning-text">${warning_text}</div>
     <div class="card-body">
       <h5 class="card-title car-make-model">${vehicle.year} ${vehicle.make} ${vehicle.model}</h5>
       <img class="card-img-top" src=${img_src} alt="Card image cap">
     </div>
-  ;
-  vehicleContainer.appendChild(vehicleDiv);
+  `;
+    vehicleContainer.appendChild(vehicleDiv);
 
-  const vehicleButtons = document.querySelectorAll(".card");
+    const vehicleButtons = document.querySelectorAll(".card");
 
-  vehicleButtons.forEach((div) => {
-    div.addEventListener("click", () => {
-      location.href = ./vehicle.html?id=${div.id};
+    vehicleButtons.forEach((div) => {
+      div.addEventListener("click", () => {
+        location.href = `./vehicle.html?id=${div.id}`;
+      });
     });
   });
-  });
-  }
+}
